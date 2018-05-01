d1cb885b9573d53d1e55b9a1019dab240bf0fae1
DHJT <dhjt11@qq.com>
2018-05-01 13:11:55 +0800

    调整了test测试类的位置，将其单独放置在一个test包中

--- src/com/dhjt/test/test.java
+++ src/com/dhjt/test/test.java
@@ -1,9 +1,15 @@
-package com.dhjt.office2html;
+package com.dhjt.test;
 
+import com.dhjt.office2html.POIWordToHtml;
+/**
+ * 测试类
+ * @author DHJT 2018年5月1日 下午1:04:20
+ *
+ */
 public class test {
 
 	public static void main(String[] args) {
-		POIWordToHtml.wordToHtml("C:/poi/test.docx", "C:/poi/image", "C:/poi/html/test.html");
+		POIWordToHtml.wordToHtml("C:/Workspaces/TestTemp/test.docx", "C:/TestTemp/poi/image", "C:/Workspaces/TestTemp/poi/html/test.html");
 //		POIWordToHtml.wordToHtml("C://poi/test2.doc", "C://poi/image", "C://poi/html/test2.html");
 //		POIExcelToHtml.excelToHtml("C://poi/test3.xlsx", "C://poi/html/test3.html", true);
 //		POIExcelToHtml.excelToHtml("C://poi/test4.xls", "C://poi/html/test4.html", true);
